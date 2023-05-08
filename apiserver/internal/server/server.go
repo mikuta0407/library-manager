8bfed1e35b176680da63797b22eefbac624c6386
mikuta0407 <flute.takkun0407@gmail.com>
2023-05-09 00:55:28 +0900

    server作成

--- apiserver/internal/server/server.go
+++ apiserver/internal/server/server.go
@@ -1,41 +1,27 @@
 package server
 
 import (
-	"fmt"
 	"log"
 	"net/http"
-	"strings"
+
+	"github.com/mikuta0407/library-manager/internal/api"
+	"github.com/mikuta0407/library-manager/internal/database"
 )
 
-func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to the HomePage!")
-	fmt.Println("Endpoint Hit: homePage")
-
-	params := getRouteParams(r)
-	fmt.Println(params)
-
-}
-
 func HandleRequests() {
-	http.HandleFunc("/list/", homePage)   // 一覧
-	http.HandleFunc("/detail/", homePage) // 詳細
-	http.HandleFunc("/create/", homePage) // レコード作成
-	http.HandleFunc("/update/", homePage) // レコード編集
-	http.HandleFunc("/detail/", homePage) // レコード削除
+	if err := database.ConnectDB("./library.db"); err != nil {
+		log.Fatalln(err)
+	}
+
+	http.HandleFunc("/list/", api.List)     // 一覧
+	http.HandleFunc("/detail/", api.Detail) // 詳細
+	http.HandleFunc("/create/", api.Create) // レコード作成
+	http.HandleFunc("/update/", api.Update) // レコード編集
+	http.HandleFunc("/delete/", api.Delete) // レコード削除
 	log.Fatal(http.ListenAndServe(":8081", nil))
+	defer database.DisconnectDB()
 }
 
 func main() {
 	HandleRequests()
-}
-
-func getRouteParams(r *http.Request) []string {
-	splited := strings.Split(r.RequestURI, "/")
-	var params []string
-	for i := 0; i < len(splited); i++ {
-		if len(splited[i]) != 0 {
-			params = append(params, splited[i])
-		}
-	}
-	return params
 }
