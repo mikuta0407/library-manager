60e79e74f240af48af3ad497695a4accfdbebb93
mikuta0407 <flute.takkun0407@gmail.com>
2023-05-09 18:24:27 +0900

    add json tag

--- apiserver/internal/models/models.go
+++ apiserver/internal/models/models.go
@@ -1,11 +1,11 @@
 package models
 
 type Item struct {
-	Id     int8
-	Title  string
-	Author string // artistを含む
-	Code   string
-	Place  string
-	Note   string
-	Image  []byte
+	Id     int8   `json:"id"`
+	Title  string `json:"title"`
+	Author string `json:"author"` // artistを含む
+	Code   string `json:"code"`
+	Place  string `json:"place"`
+	Note   string `json:"note"`
+	Image  []byte `json:"image"`
 }
